c6455e6f5936a1d67950bd65e7724f03775dba3f
George Sandoval <gssandoval09@gmail.com>
2021-11-11 19:51:26 -0600

    started adding in code into apiroutes

---
+++ routes/apiroutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const Workout = require("../models");
+
+router.post("/api/workouts", (req,res) => {
+
+}),
+router.put("/api/workouts/:id", ({body,params},res) => {
+    
+}),
+router.get("/api/workouts", (req,res) => {
+    
+}),
+router.get("/api/workouts/range", (req,res) => {
+    
+}),
+router.delete("/api/workouts", ({body},res) => {
+    
+})
+
+module.exports = router;
