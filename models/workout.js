c9f443be903d48a58965314cd04086d180a72fd2
George Sandoval <gssandoval09@gmail.com>
2021-11-11 19:38:05 -0600

    added code into workoutjs

---
+++ models/workout.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+exercises: [{
+    type: {
+        type:String,
+        required: "Enter the type of exercise"
+    },
+    name: {
+        type: String,
+        required: "Enter exercise name"
+    },
+    duration: {
+        type: Number,
+        required: "How long did you exercise?"
+    },
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+    distance: {
+        type: Number,
+    },
+},
+
+]
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
